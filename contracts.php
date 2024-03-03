e427e45ba19a409cfcba3bd838f21d338a4b72e4
Козимиров Дмитрий <kozimirovdima23@gmail.com>
2024-03-03 18:04:38 +0300

    templete
    parts

---
+++ contracts.php
@@ -0,0 +1,7 @@
+<?php 
+$page_title = 'Контрактное производство';
+$header_style = "page-header-bg-grey";
+include_once('header-page.php');
+$production_title = "";
+include_once('./template-parts/production-block.php');
+include_once('footer.php');?>
---
+++ template-parts/clients-block.php
@@ -0,0 +1,143 @@
+<section class="section clients section-clients">
+  <div class="container">
+    <div class="seporator"></div>
+    <div class="clients-wrapper">
+      <div class="clients-content">
+        <h2 class="section-title">
+          Производим аэрозольную продукцию для разных сфер
+        </h2>
+        <ul class="clients-list">
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use href="img/sprite.svg#him"></use>
+            </svg>
+            Химические производства
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use href="img/sprite.svg#car"></use>
+            </svg>
+            Автомойки
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use href="img/sprite.svg#eat"></use>
+            </svg>
+            Пищевая продукция
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use href="img/sprite.svg#brush"></use>
+            </svg>
+            Лаки и краски
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use href="img/sprite.svg#cosmetic"></use>
+            </svg>
+            Косметические средства
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use href="img/sprite.svg#car-cosm"></use>
+            </svg>
+            Автомобильная косметика
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use href="img/sprite.svg#shirt"></use>
+            </svg>
+            Косметика по уходу за одеждой
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use href="img/sprite.svg#boots"></use>
+            </svg>
+            Косметика по уходу за обувью
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use href="img/sprite.svg#bricks"></use>
+            </svg>
+            Строительные материалы
+          </li>
+          <li class="clients-list-item">
+            <svg width="30" height="30" class="clients-list-icon">
+              <use href="img/sprite.svg#more"></use>
+            </svg>
+            И многих других
+          </li>
+        </ul>
+      </div>
+      <!-- /.clients-content -->
+      <div class="clients-logo-list">
+        <a href="#" class="clinets-logo-item">
+          <picture>
+            <source type="image/webp" srcset="img/client.webp"> 
+            <source type="image/jpeg" srcset="img/client.png"> 
+            <img src="img/client.png" class="clients-logo" alt="client"> 
+          </picture>
+        </a>
+        <a href="#" class="clinets-logo-item">
+          <picture>
+            <source type="image/webp" srcset="img/client.webp"> 
+            <source type="image/jpeg" srcset="img/client.png"> 
+            <img src="img/client.png" class="clients-logo" alt="client"> 
+          </picture>
+        </a>
+        <a href="#" class="clinets-logo-item">
+          <picture>
+            <source type="image/webp" srcset="img/client.webp"> 
+            <source type="image/jpeg" srcset="img/client.png"> 
+            <img src="img/client.png" class="clients-logo" alt="client"> 
+          </picture>
+        </a>
+        <a href="#" class="clinets-logo-item">
+          <picture>
+            <source type="image/webp" srcset="img/client.webp"> 
+            <source type="image/jpeg" srcset="img/client.png"> 
+            <img src="img/client.png" class="clients-logo" alt="client"> 
+          </picture>
+        </a>
+        <a href="#" class="clinets-logo-item">
+          <picture>
+            <source type="image/webp" srcset="img/client.webp"> 
+            <source type="image/jpeg" srcset="img/client.png"> 
+            <img src="img/client.png" class="clients-logo" alt="client"> 
+          </picture>
+        </a>
+        <a href="#" class="clinets-logo-item">
+          <picture>
+            <source type="image/webp" srcset="img/client.webp"> 
+            <source type="image/jpeg" srcset="img/client.png"> 
+            <img src="img/client.png" class="clients-logo" alt="client"> 
+          </picture>
+        </a>
+        <a href="#" class="clinets-logo-item">
+          <picture>
+            <source type="image/webp" srcset="img/client.webp"> 
+            <source type="image/jpeg" srcset="img/client.png"> 
+            <img src="img/client.png" class="clients-logo" alt="client"> 
+          </picture>
+        </a>
+        <a href="#" class="clinets-logo-item">
+          <picture>
+            <source type="image/webp" srcset="img/client.webp"> 
+            <source type="image/jpeg" srcset="img/client.png"> 
+            <img src="img/client.png" class="clients-logo" alt="client"> 
+          </picture>
+        </a>
+        <a href="#" class="clinets-logo-item">
+          <picture>
+            <source type="image/webp" srcset="img/client.webp"> 
+            <source type="image/jpeg" srcset="img/client.png"> 
+            <img src="img/client.png" class="clients-logo" alt="client"> 
+          </picture>
+        </a>
+      </div>
+      <!-- /.clients-logo -->
+    </div>
+    <!-- /.clients-wrapper -->
+  </div>
+  <!-- /.container -->
+</section>
